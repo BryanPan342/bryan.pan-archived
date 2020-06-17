457745cfdec910b32c93c3e75191d467908ceac0
Bryan Pan <bryanpan342@gmail.com>
2020-06-17 01:42:34 -0700

    using anime.js

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {LoaderProps} from '../utils/LoaderUtils';
+import {LoaderProps, shoot_balls, expand_center} from '../utils/LoaderUtils';
+import {random} from '../utils/common';
 
 function Loader(props: LoaderProps){
     const theme = props.theme;
@@ -8,27 +9,54 @@ function Loader(props: LoaderProps){
         console.log('yellow')
     }
 
+    var iter:number, t_offset:number = 0;
+    for (iter = 0; iter < 8; iter++){
+        let t0:number = iter;
+        let t1:number = iter*2 + 1;
+        let t2:number = iter*2 + 2;
+        setTimeout(() =>{
+            shoot_balls(t1);
+            shoot_balls(t2);
+
+            expand_center(t0);
+        }, 1000 + t_offset);
+
+        t_offset += random(100,700);
+    }
+
+    setTimeout(()=>{
+
+    }, 1000 + t_offset + 1000);
+
     return (
-        <div className='loader fullscreen'>
-            <div className="sun">
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-            </div>
-            <div className='sun-center'/>
+        <div className='loader-wrapper'>
+            {props.theme == 'edgy-yellow' ? 
+                <div className='loader fullscreen'>
+                    <div className="sun">
+                        <span className='balls_1'/>
+                        <span className='balls_2'/>
+                        <span className='balls_3'/>
+                        <span className='balls_4'/>
+                        <span className='balls_5'/>
+                        <span className='balls_6'/>
+                        <span className='balls_7'/>
+                        <span className='balls_8'/>
+                        <span className='balls_9'/>
+                        <span className='balls_10'/>
+                        <span className='balls_11'/>
+                        <span className='balls_12'/>
+                        <span className='balls_13'/>
+                        <span className='balls_14'/>
+                        <span className='balls_15'/>
+                        <span className='balls_16'/>
+                    </div>
+                    <div className='sun-center'/>
+                </div>
+            :
+                <div>
+                    Unfortunate
+                </div>
+            }
         </div>
     );
 }
--- src/utils/LoaderUtils.ts
+++ src/utils/LoaderUtils.ts
@@ -1,3 +1,41 @@
+import anime from 'animejs';
+
 export interface LoaderProps {
     theme: string
+}
+
+const ball_offsets:number[][] = [[0, 165], [330, 165], 
+                                [165, 330], [165, 0], 
+                                [48.33, 281.67], [281.67, 48.33], 
+                                [48.33, 48.33], [281.67, 281.67],
+                                [12.56, 228.14], [317.44, 101.86],
+                                [101.86, 317.44], [228.14, 12.56],
+                                [228.14, 317.44], [101.86, 12.56],
+                                [317.44, 228.14], [12.56, 101.86]]
+
+export function shoot_balls(n:number){
+    let tgt:string = '.balls_' + n.toString();
+    let tp:string = ball_offsets[n-1][0].toString() + 'px';
+    let lft:string = ball_offsets[n-1][1].toString() + 'px';
+    anime({
+        targets: tgt,
+        top: tp,
+        left: lft,
+        easing: 'easeOutQuad',
+        duration: 1000,
+    });
+}
+
+export function expand_center(iter:number){
+    console.log(iter)
+    let tgt:string = '.sun-center';
+    let s:string = (30 + iter*30) + 'px';
+    console.log(s)
+    anime({
+        targets: tgt,
+        width: s,
+        height: s,
+        easing: 'easeOutQuad',
+        duration: 1000,
+    });
 }
