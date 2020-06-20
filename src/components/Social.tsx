29dedbfa14958933cf352fbe6f4b76188abf0a30
Bryan Pan <bryanpan342@gmail.com>
2020-06-20 01:05:44 -0700

    change social to open in another tab

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -9,11 +9,11 @@ import facebook from  '../assets/facebook.svg';
 function Social(){
     return(
         <div className='social'>
-            <a href="https://www.linkedin.com/in/blpan/"><img src={linkedin}/></a>
-            <a href="https://github.com/BryanPan342"><img src={github}/></a>
-            <a href="https://groundsofthecity.com"><img src={blog}/></a>
-            <a href="https://www.instagram.com/bryanthesnob/"><img src={instagram}/></a>
-            <a href="https://www.facebook.com/bryanpan342"><img src={facebook}/></a>
+            <a target="_blank" href="https://www.linkedin.com/in/blpan/"><img src={linkedin}/></a>
+            <a target="_blank" href="https://github.com/BryanPan342"><img src={github}/></a>
+            <a target="_blank" href="https://groundsofthecity.com"><img src={blog}/></a>
+            <a target="_blank" href="https://www.instagram.com/bryanthesnob/"><img src={instagram}/></a>
+            <a target="_blank" href="https://www.facebook.com/bryanpan342"><img src={facebook}/></a>
         </div>
     );
 }
